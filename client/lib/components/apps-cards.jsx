398bee289daa5e777f28353bafc93e5c13ff988a
yenpraja610 <yen.a@me.com>
2017-08-01 18:22:30 -0400

    Implement avatar and margin Implement avatar in apps-cards add margin to even space between navbar-headings-and cards.

--- client/lib/components/apps-cards.jsx
+++ client/lib/components/apps-cards.jsx
@@ -19,11 +19,10 @@ class  AppsCards extends React.Component {
             <Card>
               <CardTitle title="Card title" subtitle="Card subtitle" />
               <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam
-                sed pellentesque. Aliquam dui mauris, mattis quis lacus id,
-                pellentesque lobortis odio.
+                <CardHeader
+                        title="App Name"
+                                    avatar="images/jsa-128.jpg"
+                                        />
               </CardText>
               <OptionsForm />
             </Card>
